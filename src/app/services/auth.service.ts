ba342b00bf74491ae368e5cea1745b900f867353
turongsosoft <truongg166@gmail.com>
2025-08-04 21:08:20 +0700

    .

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+interface LoginResponse {
+  user: any; // Thay thế 'any' bằng kiểu dữ liệu phù hợp với response của bạn
+  token?: string; // Nếu có token trả về, bạn có thể theo dõi nó
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'https://localhost:3301/login'; // Đường dẫn API của bạn
+  private apiUrl = 'https://localhost:3301/auth/login'; // Đường dẫn API của bạn
 
   constructor(private http: HttpClient) {}
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(this.apiUrl, { username, password });
+  login(username: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(this.apiUrl, { username, password });
   }
 }
