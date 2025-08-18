2937762ddb763bcc32a98872392e15e8d49aee31
turongsosoft <truongg166@gmail.com>
2025-08-18 21:08:53 +0700

    mananger view update

--- src/app/manager-component/manager-component.ts
+++ src/app/manager-component/manager-component.ts
@@ -1,16 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { MaterialModule } from '../material.module';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface PeriodicElement {
+  id?: number;
+  name: string;
+  office: string;
+  position: string;
+}
+
+const ELEMENT_DATA: Omit<PeriodicElement, 'id'>[] = [
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+  { name: 'Trung', position: 'Nhân viên', office: 'Kỹ thuật' },
+];
 @Component({
   selector: 'app-manager-component',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './manager-component.html',
   styleUrl: './manager-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ManagerComponent implements OnInit {
-  dataSource: any[] = []; // Placeholder for data source
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['STT', 'name', 'office', 'position', 'options'];
 
   ngOnInit(): void {
     // Initialization logic for the manager component
     console.log('ManagerComponent initialized');
+    this.dataSource = new MatTableDataSource<PeriodicElement>(
+      ELEMENT_DATA.map((element, index) => ({
+        id: index + 1, // Gán id cho mỗi phần tử
+        ...element,
+      }))
+    );
+  }
+
+  addEmployee(): void {
+    return;
+  }
+
+  importExcel(): void {
+    return;
+  }
+
+  exportExcel(): void {
+    return;
+  }
+
+  editEmployee(id: string): void {
+    console.log(`Editing employee with id: ${id}`);
+  }
+
+  deleteEmployee(id: string): void {
+    console.log(`Deleting employee with id: ${id}`);
   }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,6 +4,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule, MatCardTitle } from '@angular/material/card';
 import { MatFormField, MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
 
 const modules = [
   MatCardModule,
@@ -13,6 +16,9 @@ const modules = [
   MatCardTitle,
   ReactiveFormsModule,
   MatListModule,
+  MatTableModule,
+  MatPaginator,
+  MatIconModule,
 ];
 
 @NgModule({
